d2786b25f130528c34107975964fa907c4e0ed2e
Araden14 <arnaud-danselme@hotmail.com>
2025-11-24 14:11:48 +0100

    feat : migrate cmd

--- cmd/cli/migrate.go
+++ cmd/cli/migrate.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"log"
 
-	cmd2 "github.com/axellelanca/urlshortener/cmd"
+	"github.com/axellelanca/urlshortener/internal/config"
 	"github.com/axellelanca/urlshortener/internal/models"
 	"github.com/spf13/cobra"
-	"gorm.io/driver/sqlite" // Driver SQLite pour GORM
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	// Driver SQLite pour GORM
 )
 
 // MigrateCmd représente la commande 'migrate'
@@ -19,19 +20,25 @@ var MigrateCmd = &cobra.Command{
 et exécute les migrations automatiques de GORM pour créer les tables 'links' et 'clicks'
 basées sur les modèles Go.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO : Charger la configuration chargée globalement via cmd.cfg
-
-		// TODO 2: Initialiser la connexion à la BDD
-
+		cfg, err := config.LoadConfig()
+		if err != nil {
+			log.Fatalf("FATAL: Échec du chargement de la configuration: %v", err)
+		}
+		dsn := cfg.Database.Name
+		db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+		if err != nil {
+			log.Fatalf("FATAL: Échec de l'obtention de la base de données SQL sous-jacente: %v", err)
+		}
 		sqlDB, err := db.DB()
 		if err != nil {
 			log.Fatalf("FATAL: Échec de l'obtention de la base de données SQL sous-jacente: %v", err)
 		}
-		// TODO Assurez-vous que la connexion est fermée après la migration grâce à defer
-
-		// TODO 3: Exécuter les migrations automatiques de GORM.
+		defer sqlDB.Close()
 		// Utilisez db.AutoMigrate() et passez-lui les pointeurs vers tous vos modèles.
-
+		err = db.AutoMigrate(&models.Link{}, &models.Click{})
+		if err != nil {
+			log.Fatalf("FATAL: Échec de la migration de la base de données: %v", err)
+		}
 		// Pas touche au log
 		fmt.Println("Migrations de la base de données exécutées avec succès.")
 	},
