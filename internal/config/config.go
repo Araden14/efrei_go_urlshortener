7d10c5623d7e4289bbeec6933a6aafe9af5be712
Araden14 <arnaud-danselme@hotmail.com>
2025-11-24 10:13:54 +0100

    done : config file config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,7 +10,29 @@ import (
 // TODO Créer Config qui est la structure principale qui mappe l'intégralité de la configuration de l'application.
 // Les tags `mapstructure` sont utilisés par Viper pour mapper les clés du fichier de config
 // (ou des variables d'environnement) aux champs de la structure Go.
+
+type Server struct {
+	Port    int
+	BaseURL string
+}
+
+type Database struct {
+	Name string
+}
+
+type Analytics struct {
+	BufferSize  int
+	WorkerCount int
+}
+
+type Monitor struct {
+	IntervalMinutes int
+}
 type Config struct {
+	Server
+	Database
+	Analytics
+	Monitor
 }
 
 // LoadConfig charge la configuration de l'application en utilisant Viper.
@@ -19,8 +41,11 @@ type Config struct {
 func LoadConfig() (*Config, error) {
 	// TODO Spécifie le chemin où Viper doit chercher les fichiers de config.
 	// on cherche dans le dossier 'configs' relatif au répertoire d'exécution.
+	var path string = "../configs/"
+	viper.AddConfigPath(path)
 
 	// TODO Spécifie le nom du fichier de config (sans l'extension).
+	viper.SetConfigFile("config")
 
 	// TODO Spécifie le type de fichier de config.
 	viper.SetConfigType("yaml")
@@ -29,11 +54,25 @@ func LoadConfig() (*Config, error) {
 	// Ces valeurs seront utilisées si les clés correspondantes ne sont pas trouvées dans le fichier de config
 	// ou si le fichier n'existe pas.
 	// server.port, server.base_url etc.
+	viper.SetDefault("server.port", 8080)
+	viper.SetDefault("server.base_url", "http://localhost:8080")
+	viper.SetDefault("database.name", "url_shortener.db")
+	viper.SetDefault("analytics.buffer_size", 1000)
+	viper.SetDefault("analytics.worker_count", 5)
+	viper.SetDefault("monitor.interval_minutes", 5)
 
 	// TODO : Lire le fichier de configuration.
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
 
 	// TODO 4: Démapper (unmarshal) la configuration lue (ou les valeurs par défaut) dans la structure Config.
 	var cfg Config
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config: %w", err)
+	}
 
 	// Log  pour vérifier la config chargée
 	log.Printf("Configuration loaded: Server Port=%d, DB Name=%s, Analytics Buffer=%d, Monitor Interval=%dmin",
